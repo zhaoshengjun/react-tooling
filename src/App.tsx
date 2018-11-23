cb1f880dffb39d71e8dee78815c369bd893a121f
Sam Zhao <zhao.shengjun@gmail.com>
2018-11-23 22:58:46 +1000

    use button

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { Heading } from "./Heading";
+import { Button } from "./Button";
 
 class App extends Component {
   render() {
     return (
       <div className="app">
         <Heading>My App</Heading>
+        <Button />
       </div>
     );
   }
